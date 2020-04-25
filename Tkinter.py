d5cd81dcf3db77dee840155bd5e625f4db7ad26a
Isabel Lomas <i@sabel.dev>
2020-04-24 17:05:45 -0700

    make Tkinter.py great again

--- Tkinter.py
+++ Tkinter.py
@@ -1,33 +1,57 @@
-from tkinter import *
-from Main import *
+from tkinter import Tk, StringVar, Label, Button, Entry
+import tkinter
+from Main import GimkitBot
 import time
 root = Tk()
 error_var = StringVar()
-def stuff():
-    print(code.get())
+
+def startcmd():
     if code.get() == '':
-        error_var.set("You didn't enter a code. Try entering a code")
-    else:
-        bot = GimkitBot()
-        bot.join(code.get())
-        time.sleep(2)
-        bot.start()
+        return error_var.set("You didn't enter a code. Try entering a code.")
+    bot = GimkitBot()
+    bot.join(code.get())
+    time.sleep(2)
+    bot.start()
 
 
+class EntryWithPlaceholder(tkinter.Entry):
+    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
+        super().__init__(master)
+
+        self.placeholder = placeholder
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.foc_in)
+        self.bind("<FocusOut>", self.foc_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self):
+        self.insert(0, self.placeholder)
+        self['fg'] = self.placeholder_color
+
+    def foc_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('0', 'end')
+            self['fg'] = self.default_fg_color
+
+    def foc_out(self, *args):
+        if not self.get():
+            self.put_placeholder()
 
 root.title("Gimkit Bot")
 root.geometry('400x300')
 root.iconbitmap(r'Icon.ico')
 root.resizable(False, False)
-#root.configure(background = 'white')
-title = Label(root, text ="Welcome to the Gimkit Bot!")
-title.pack(pady= 10)
+title = Label(root, text = "Welcome to the Gimkit Bot!")
+title.pack(pady = 10)
 title.config(font = ("arial", 20))
 
-code = Entry(root)
+code = EntryWithPlaceholder(root, placeholder="Game Code")
 code.pack(padx = 10, pady = 10)
 
-start = Button(root, text = "Start", command = stuff)
+start = Button(root, text = "Start", command = startcmd)
 start.pack(pady = 10)
 
 error_text = Label(root, textvariable = error_var)
